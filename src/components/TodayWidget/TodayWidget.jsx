db003241e7041a13016ec421056f59516c1e891a
Misha_Lipovka <miwist@mail.ru>
2023-07-02 00:38:02 +0500

    add dnd

--- src/components/TodayWidget/TodayWidget.jsx
+++ src/components/TodayWidget/TodayWidget.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import styles from './TodayWidget.module.css';
-
+import React, { useEffect, useState } from "react";
+import styles from "./TodayWidget.module.css";
+import Draggable from "react-draggable";
 // const greeting = {morning, afternoon, evening}
 const TodayWidget = () => {
   const [date, setDate] = useState(new Date());
@@ -10,35 +10,38 @@ const TodayWidget = () => {
   });
 
   function dragStartHandler(e) {
-    e.target.style.backgroundColor = 'red';
+    e.target.style.backgroundColor = "red";
   }
 
   function dragEndHandler(e) {
-    console.log('end', e.target);
+    console.log("end", e.target);
     e.preventDefault();
   }
 
   return (
-    <div
-      draggable
-      onDragStart={dragStartHandler}
-      onDragEnd={dragEndHandler}
-      className={styles.today}>
-      <div className={styles.first}>
-        <p className={styles.heading}>Good afternoon</p>
-        <p className={styles.heading}>
-          {date.toLocaleTimeString('en-US', {
-            timeStyle: 'short',
-          })}
-        </p>
+    <Draggable
+      onStart={() => console.log("Перетаскивание началось")}
+      onStop={() => console.log("Перетаскивание закончилось")}
+      onDrag={() => console.log("Перетаскивание в процессе")}
+    >
+      <div className={styles.move}>
+        {" "}
+        <div className={styles.first}>
+          <p className={styles.heading}>Good afternoon</p>
+          <p className={styles.heading}>
+            {date.toLocaleTimeString("en-US", {
+              timeStyle: "short",
+            })}
+          </p>
+        </div>
+        <p className={styles.paragraph}>{` It’s ${date.toLocaleString("en-US", {
+          weekday: "long",
+          month: "long",
+          day: "2-digit",
+        })}`}</p>
       </div>
-
-      <p className={styles.paragraph}>{` It’s ${date.toLocaleString('en-US', {
-        weekday: 'long',
-        month: 'long',
-        day: '2-digit',
-      })}`}</p>
-    </div>
+    </Draggable>
+  
   );
 };
 
