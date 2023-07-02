b01b6ab9c8bbfee4e439b1c81e1ea58a4885328b
soinanim <kristina07770@gmail.com>
2023-07-02 21:31:42 +0600

    feat: added sounds

--- src/components/Button/ButtonCircle.jsx
+++ src/components/Button/ButtonCircle.jsx
@@ -1,15 +1,63 @@
-import React from "react";
-import styles from "./ButtonCircle.module.css";
+import React, { useEffect, useState } from 'react';
+import styles from './ButtonCircle.module.css';
 
-const ButtonCircle = ({name, left, top}) => {
+const ButtonCircle = ({ name, left, top }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [volume, setVolume] = useState(0.5);
+
+  const playAudio = () => {
+    setIsPlaying((state) => !state);
+  };
+
+  const changeVolume = (e) => {
+    setVolume(e.target.value);
+  };
+
+  useEffect(() => {
+    let audioBirds = document.querySelector('.audio-birds');
+    let audioRain = document.querySelector('.audio-rain');
+
+    if (name === 'birds') {
+      isPlaying ? audioBirds.play() : audioBirds.pause();
+      audioBirds.volume = volume;
+    }
+
+    if (name === 'rain') {
+      isPlaying ? audioRain.play() : audioRain.pause();
+      audioRain.volume = volume;
+    }
+  }, [isPlaying, name, volume]);
+
   return (
-    <div className={styles.popoverAction} style={{left: left +'%', top: top + '%'}}>
-      <div className={styles.circleHover}>
-        <div></div>
+    <div
+      className={styles.popoverAction}
+      style={{ left: left + '%', top: top + '%' }}>
+      <div onClick={playAudio} className={styles.circleHover}>
+        <audio src='audio/birds.mp3' loop className='audio-birds'></audio>
+        <audio src='audio/rain.mp3' loop className='audio-rain'></audio>
       </div>
+
       <div className={styles.popoverCard}>
         <h5>{name}</h5>
-        <input type="range" min="0" max="100"   name="value" className={styles.input}/>
+        {isPlaying && (
+          <div className={styles.levelInput}>
+            <div className={styles.trackFull}>
+              <div
+                style={{ flex: `${volume} 1 0%`, display: 'initial' }}
+                className={styles.track}></div>
+            </div>
+            <input
+              defaultValue={volume}
+              type='range'
+              min='0'
+              max='1'
+              step='0.01'
+              name='value'
+              className={styles.input}
+              onChange={changeVolume}
+            />
+          </div>
+        )}
       </div>
     </div>
   );
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import Header from "../Header/Header";
-import TodayWidget from "../TodayWidget/TodayWidget";
-import ButtonCircle from "../Button/ButtonCircle";
+import React, { useState } from 'react';
+import Header from '../Header/Header';
+import TodayWidget from '../TodayWidget/TodayWidget';
+import ButtonCircle from '../Button/ButtonCircle';
 
-import styles from "./Main.module.css";
+import styles from './Main.module.css';
 
 const Main = () => {
   const [openWidget, setOpenWidget] = useState(false);
@@ -11,18 +11,18 @@ const Main = () => {
   return (
     <div className={styles.main}>
       <Header openWidget={openWidget} setOpenWidget={setOpenWidget} />
+      {openWidget && <TodayWidget />}
 
       <video
-        src="/videos/room-night.mp4"
+        src='/videos/room-night.mp4'
         autoPlay
         loop
         muted
         playsInline
-        className={styles.video}
-      ></video>
-      {openWidget && <TodayWidget />}
-      <ButtonCircle name="Forest rain" left="5" top="30" />
-      <ButtonCircle name="Window birds" left="72" top="40" />
+        className={styles.video}></video>
+
+      <ButtonCircle name='rain' left='5' top='30' />
+      <ButtonCircle name='birds' left='72' top='40' />
     </div>
   );
 };
